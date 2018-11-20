2c24021898588094cef0604df5b073e482856ff5
Rahul <44020002+rahulmadaan@users.noreply.github.com>
2018-11-20 19:13:37 +0530

    Enhanced test framework to mocha:filterTests.js
    Enhanced the test framework of test file: filterTests.js.
    using mocha in filterTests.js and implementing the same framework in
    other test files too.

--- test/filterTests.js
+++ test/filterTests.js
@@ -1,5 +1,5 @@
 let { equal,deepEqual } = require('assert');
-let { filter,map } = require('../src/library.js');
+let { filter } = require('../src/library.js');
 
 //----------------------------------------------------------
 // functions required for testing filter function.
@@ -9,34 +9,37 @@ const isEven = function(number) {
 const isOdd = function(number) {
   return number%2 != 0 ;
 }
-//-------------------------------------------------------------
-const testFilter = function() {
-
-  const test = function(testFunction,input,expectedOutput) {
-    deepEqual(filter(input,testFunction),expectedOutput);
-  }
-
-  // empty input
-  test(isOdd,[],[]);
-
-  // with only one input
-  test(isOdd,[1],[1]);
-  test(isEven,[1],[]);
-
-  // positive numbers
-  test(isOdd,[11,12,13,20,21,24],[11,13,21]);
-  test(isEven,[11,12,14,20,21,24],[12,14,20,24])
-
-  // negative values
-  test(isOdd,[-1,-2,-3,-4],[-1,-3]);
-  test(isEven,[0,-2,-4,-7,-12,-15],[0,-2,-4,-12]);
-
-  // array of array as input
-  test(isEven,[[1,2,3],[4,5,6],[7,8,9]],[]);
-
-  // array of object as input
-  test(isEven,[{1:1,2:2},{3:3,4:4}],[]);
-
-  console.log('All filter tests passed!!!');
+const test = function(testFunction,input,expectedOutput) {
+  deepEqual(filter(input,testFunction),expectedOutput);
 }
-testFilter();
+//-------------------------------------------------------------
+
+describe('Testing Filter function',function() {
+
+  it('should return empty array on no input',function() {
+    test(isOdd,[],[]);
+  });
+
+  it('with only one input',function(){
+    test(isOdd,[1],[1]);
+    test(isEven,[1],[]);
+  });
+
+  it('tests with positive numbers',function(){
+    test(isOdd,[11,12,13,20,21,24],[11,13,21]);
+    test(isEven,[11,12,14,20,21,24],[12,14,20,24])
+  });
+
+  it('tests with negative numbers',function(){
+    test(isOdd,[-1,-2,-3,-4],[-1,-3]);
+    test(isEven,[0,-2,-4,-7,-12,-15],[0,-2,-4,-12]);
+  });
+  it('input as array of array',function(){
+    test(isEven,[[1,2,3],[4,5,6],[7,8,9]],[]);
+  });
+
+  it('input as array of objects',function(){
+    test(isEven,[{1:1,2:2},{3:3,4:4}],[]);
+
+  });
+});
